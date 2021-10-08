5d47cc5bff83f4dadfa70677f133799450e3cf1e
SheilaLCC <slcmj@hotmail.com>
2021-10-08 18:09:23 -0300

    fazendo calculadora

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -4,12 +4,13 @@ import br.com.dio.model.Gato;
 
 public class primeiroprograma {
     public static void main(String[] args) {
-        Gato gato = new Gato ();
+        Gato gato = new Gato();
         System.out.println(gato);
 
+    }
                        /*int a=5;
         int b=3;
         System.out.println("Hello world! " + (a+b));*/
 
-    }
 }
+
