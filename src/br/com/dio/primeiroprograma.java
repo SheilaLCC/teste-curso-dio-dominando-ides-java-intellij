346625357d727630ed7717976fe4002f05f3a567
SheilaLCC <slcmj@hotmail.com>
2021-10-08 13:43:16 -0300

    alterando

--- src/br/com/dio/primeiroprograma.java
+++ src/br/com/dio/primeiroprograma.java
@@ -6,7 +6,8 @@ public class primeiroprograma {
     public static void main(String[] args) {
         Gato gato = new Gato ();
         System.out.println(gato);
-        /*int a=5;
+
+                       /*int a=5;
         int b=3;
         System.out.println("Hello world! " + (a+b));*/
 
